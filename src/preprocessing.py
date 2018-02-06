b85872e3bb71b2a15bfe56ce77c391228e4d048e
Kevin Berry <kpberry11@gmail.com>
2018-02-06 18:42:28 -0500

    Demos output

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -3,5 +3,9 @@ import pandas as pd
 def read_data(filename, instance='text', label='airline_sentiment'):
     df = pd.read_csv(filename)
     labels = df[label].values
-    instances = df[instances].values
+    instances = df[instance].values
     return (instances, labels)
+
+instances, labels = read_data('../data/Tweets.csv')
+for i in range(10):
+    print(instances[i], labels[i])
