ec48ac8d8810898c3895554a59a7e9ce08c8aaa2
Kevin Berry <kpberry11@gmail.com>
2018-02-13 18:50:02 -0500

    Adds p word | label

--- src/naive_bayes.py
+++ src/naive_bayes.py
@@ -13,3 +13,14 @@ def get_label_word_counts(texts, labels, label):
         if y == label:
             counter.update(x)
     return defaultdict(float, counter)
+
+def estimate_prob_words_given_label(texts, labels, label, smoothing, vocab):
+    # P(w | L) = P(w AND L) / P(L)
+    conditional_probs = defaultdict(float)
+
+    label_word_counts = get_label_word_counts(texts, labels, label)
+    label_count = sum(label_word_counts.values()) + len(vocab) * smoothing
+    for word in vocab:
+        word_count = label_word_counts[word]
+        conditional_probs[word] = log((word_count + smoothing) / label_count)
+    return conditional_probs
